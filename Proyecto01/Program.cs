9fd7d98f439910af4492509c183beaf014ce6d27
adriana <adriana.solis.flores.123@gmail.com>
2025-09-03 10:30:53 -0500

    el segundo commit

--- Proyecto01/Program.cs
+++ Proyecto01/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace Proyecto01
 {
@@ -10,6 +10,16 @@ namespace Proyecto01
     {
         static void Main(string[] args)
         {
+            Console.Write("Hola, ¿cuál es tu nombre?");
+            string nombre = Console.ReadLine();
+
+            Console.Write("¿Cuál es tu edad?: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("¿Cuál es tu estatura?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2} ");
         }
     }
 }
